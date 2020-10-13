525821cd0e616d111a71efa88292a1c369e3f4a6
copestylin <67732811+copestylin@users.noreply.github.com>
2020-10-13 11:44:57 +0800

    Add files via upload
    - git push in terminal throwing errors again, so uploading manually through github.com
    - gifs and konami working (challenge 3 + 4)

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 //marc copes
 
+
+
 const form = document.getElementById("form");
-
-
 
 function submitEvent(event) {
     event.preventDefault();
@@ -33,7 +33,7 @@ function submitEvent(event) {
         .then(function(result) {
 
             let html = "<h2>"+char.name+"</h2>"
-            html += "<ul>"
+            html += "<ul class=\"properties\">"
             html += "<li>homeworld url: "+char.homeworld+"</li>"
             html += "<li>homeworld name: "+result.result.properties.name+"</li>"
             html += "<li>height: "+char.height+"</li>"
@@ -44,12 +44,14 @@ function submitEvent(event) {
 
             //add gif
 
-            fetch("https://api.giphy.com/v1/gifs/search?api_key=J1noCVxuNpIkjrmWc9LZUCtzfUezIF8D&q="+char.Name+"&limit=1&offset=0&rating=g&lang=en")
+            fetch("https://api.giphy.com/v1/gifs/search?api_key=J1noCVxuNpIkjrmWc9LZUCtzfUezIF8D&q="+char.name+"&limit=1&offset=0&rating=g&lang=en")
             .then(function(response) {
                 return response.json()
             })
             .then(function(result) {
-                console.log(result.data[0].url);
+                let img = document.createElement("img");
+                img.src = result.data[0].images.original.url;
+                characterDiv.appendChild(img);
             });
 
 
@@ -60,3 +62,63 @@ function submitEvent(event) {
 }
 
 form.addEventListener("submit", submitEvent);
+
+
+let konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a', 'enter'];
+
+let userSequence = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
+
+let currentCount = 0;
+
+document.addEventListener('keyup', event => {
+
+    document.activeElement.blur();
+
+    //add next keystroke to userSequence
+    const key = event.key.toLowerCase();
+    userSequence[currentCount] = key;
+    currentCount += 1;
+
+    console.log(userSequence);
+    console.log(currentCount);
+
+    //check if userSequence is correct so far
+    for (let i=1; i <= currentCount; i++) {
+        if (userSequence[i-1] === konamiCode[i-1]) {
+        } else {
+            //if it isn't, reset userSequence and currentCount
+            userSequence = ['null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null', 'null'];
+            currentCount = 0;
+            console.log("Bad luck!");
+            console.log(userSequence);
+            break;
+            
+        }
+
+        //if I get to 11 successful presses, success!
+        if (currentCount === 11) {
+            console.log("KONAMI");
+            
+            fetch("https://api.giphy.com/v1/gifs/search?api_key=J1noCVxuNpIkjrmWc9LZUCtzfUezIF8D&q=jarjar&limit=1&offset=0&rating=g&lang=en")
+            .then(function(response) {
+                return response.json()
+            })
+            .then(function(result) {
+                document.body.innerHTML = "";
+                let jarJar = document.createElement("img");
+                jarJar.src = result.data[0].images.original.url;
+                jarJar.style.height="100%";
+                jarJar.style.width="100%";
+                jarJar.style.position = "absolute";
+                jarJar.style.top="0px";
+                jarJar.style.left="0px";
+                document.body.appendChild(jarJar);
+            });
+
+            //document.pageDiv.blur(); - how to use?
+        }
+    }
+
+
+   
+});
